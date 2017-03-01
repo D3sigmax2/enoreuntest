9f177475dc877dd03652d660456272d1aefc8500
D3sigmax2 <vonmuhlenen.a@gmail.com>
2017-03-01 09:43:17 +0100

    little change

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args){
 		String text = "test";
-		String text2 = "hello";
+		String text2 = "salut";
 		Thread_person person1 = new Thread_person(text);
 		Thread_person person = new Thread_person(text2);
 		person.start();
